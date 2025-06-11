b0d9cfec6a733a2dc15b79fe02341fb86d6e543f
Piyush Pratap Singh <mbappe6789@gmail.com>
2025-06-12 00:44:34 +0530

    pipeline implementation

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -41,7 +41,44 @@ class ModelTrainer:
                                             "AdaBoost": AdaBoostRegressor(),
                                             "K-Nearest Neighbors": KNeighborsRegressor(),
                                     }
-                    model_report:dict = evaluate_model(X_train=X_train,y_train=y_train,X_test=X_test,y_test=y_test,models = model_dict)
+                    params={
+                "Decision Tree": {
+                    'criterion':['squared_error', 'friedman_mse', 'absolute_error', 'poisson'],
+                    # 'splitter':['best','random'],
+                    # 'max_features':['sqrt','log2'],
+                },
+                "Random Forest":{
+                    # 'criterion':['squared_error', 'friedman_mse', 'absolute_error', 'poisson'],
+                 
+                    # 'max_features':['sqrt','log2',None],
+                    'n_estimators': [8,16,32,64,128,256]
+                },
+                "Gradient Boosting":{
+                    # 'loss':['squared_error', 'huber', 'absolute_error', 'quantile'],
+                    'learning_rate':[.1,.01,.05,.001],
+                    'subsample':[0.6,0.7,0.75,0.8,0.85,0.9],
+                    # 'criterion':['squared_error', 'friedman_mse'],
+                    # 'max_features':['auto','sqrt','log2'],
+                    'n_estimators': [8,16,32,64,128,256]
+                },
+                "Linear Regression":{},
+                
+                "AdaBoost":{
+                    'learning_rate':[.1,.01,0.5,.001],
+                    # 'loss':['linear','square','exponential'],
+                    'n_estimators': [8,16,32,64,128,256]
+                },
+                "K-Nearest Neighbors": {
+                                'n_neighbors': [3, 5, 7, 9, 11],
+                                'weights': ['uniform', 'distance'],
+                                'algorithm': ['auto', 'ball_tree', 'kd_tree', 'brute'],
+                                'leaf_size': [20, 30, 40],
+                                'p': [1, 2]  # 1 for Manhattan, 2 for Euclidean
+                }
+
+                
+            }
+                    model_report:dict = evaluate_model(X_train=X_train,y_train=y_train,X_test=X_test,y_test=y_test,models = model_dict,params=params)
                 
                     best_model_score = max(sorted(model_report.values()))
                     best_model_score_name = list(model_report.keys())[list(model_report.values()).index(best_model_score)]
--- src/utils.py
+++ src/utils.py
@@ -13,6 +13,7 @@ from sklearn.tree import DecisionTreeRegressor
 # from xgboost import XGBRegressor
 from src.exception import CustomException
 from src.logger import logging
+from sklearn.model_selection import GridSearchCV
 
 
 from dataclasses import dataclass
@@ -27,11 +28,16 @@ def save_obj(file_path,obj):
     except Exception as e:
         raise CustomException(sys,e)
     
-def evaluate_model(X_train,y_train,X_test,y_test,models):
+def evaluate_model(X_train,y_train,X_test,y_test,models,params):
     try:
         report = {}
         for i in range(len(list(models))):
             model = list(models.values())[i]
+            param = params[list(models.keys())[i]]
+            gs = GridSearchCV(model,param,cv=3)
+            gs.fit(X_train,y_train)
+
+            model.set_params(**gs.best_params_)
             model.fit(X_train,y_train)
             y_train_pred = model.predict(X_train)
             y_test_pred = model.predict(X_test)
@@ -43,3 +49,12 @@ def evaluate_model(X_train,y_train,X_test,y_test,models):
         return report
     except Exception as e:
         raise CustomException(e,sys)
+    
+def load_object(file_path):
+        try:
+
+            with open(file_path,'rb') as file_obj:
+                logging.info('Loading of pickle file happened succesfully')
+                return dill.load(file_obj)
+        except Exception as e:
+            raise CustomException(e,sys)
